8941121757c37f9ae2f23c56cd6b9a5a8aef07ca
Nikita Bert <n.bert@ya.ru>
2022-12-19 22:32:55 +0300

    Initial commit

---
+++ src/test/resources/scripts/outer/NotificationRestController/getNotificationPage_ViewedTest/BeforeTest.sql
@@ -0,0 +1,4 @@
+INSERT INTO notifications(id, recipient_id, text, created_date, is_viewed)
+VALUES
+    (1, 1, 'notification_text_1', '2022-01-12', false),
+    (2, 1, 'notification_text_2', '2022-01-31', true);
